2401a70690646f9f062d43a11c6e0ed6c98798cd
vojtechhuser <vojtech.huser@gmail.com>
2016-04-26 13:39:18 -0400

    some more comments added

---
+++ redshift-iris_parameterized_5.sql
@@ -0,0 +1,105 @@
+/*********************************************************************************
+# Copyright 2015 Observational Health Data Sciences and Informatics
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+********************************************************************************/
+
+/************************
+
+script to evaluate CDM repository
+
+last revised: July 21 2015
+
+author:  Vojtech Huser, Marc Suchard
+
+description:
+
+*************************/
+
+--start of analysis
+
+-- Use CTEs as suggest by SqlRender vignette so no temporary tables nor Oracle write-permissions are necessary
+
+WITH
+iris_event AS ( --- CTE
+    select
+        10 AS analysis_id,
+        CAST(a.cnt AS BIGINT) AS result
+        --CAST('count of events' AS VARCHAR) AS explanation
+FROM (
+        select
+            (select COUNT(*)   from mdcd_v5.person)
+            +(select COUNT(*)  from mdcd_v5.observation)
+            +(select COUNT(*)  from mdcd_v5.condition_occurrence)
+            +(select COUNT(*)  from mdcd_v5.drug_exposure)
+            +(select COUNT(*)  from mdcd_v5.visit_occurrence)
+            +(select COUNT(*)  from mdcd_v5.death)
+            +(select COUNT(*)  from mdcd_v5.observation)
+            +(select COUNT(*)  from mdcd_v5.procedure_occurrence) cnt
+    ) a
+),
+iris_dx_rx AS ( --- CTE
+    select
+        11 AS analysis_id,
+        CAST(a.cnt AS BIGINT) AS result
+--        CAST('count of patients with at least 1 Dx and 1 Rx' AS VARCHAR) AS explanation
+    FROM (
+        select COUNT(*) cnt from (
+            select distinct person_id from mdcd_v5.condition_occurrence
+            intersect
+            select distinct person_id from mdcd_v5.drug_exposure
+        ) b
+    ) a
+),
+
+iris_dx_proc AS ( --- CTE
+    select
+        12 AS analysis_id,
+        CAST(a.cnt AS BIGINT) AS result
+        --CAST('count of patients with at least 1 Dx and 1 Proc' AS VARCHAR) AS explanation
+    FROM (
+        select COUNT(*) cnt from (
+            select distinct person_id from mdcd_v5.condition_occurrence
+            intersect
+            select distinct person_id from mdcd_v5.procedure_occurrence
+        ) b
+    ) a
+),
+
+iris_obs_dx_rx AS ( --- CTE
+    select
+        13 AS analysis_id,
+        CAST(a.cnt AS BIGINT) AS result
+--        CAST('count of patients with at least 1 Meas, 1 Dx and 1 Rx' AS VARCHAR) AS explanation
+    FROM (
+        select COUNT(*) cnt from (
+            select distinct person_id from mdcd_v5.measurement
+            intersect
+            select distinct person_id from mdcd_v5.condition_occurrence
+            intersect
+            select distinct person_id from mdcd_v5.drug_exposure
+        ) b
+    ) a
+)
+-- a single select returns concatenated CTEs
+SELECT  analysis_id, result as count_value FROM (
+    SELECT * FROM iris_event
+    UNION ALL
+    SELECT * FROM iris_dx_rx
+    UNION ALL
+    SELECT * FROM iris_dx_proc
+    UNION ALL
+    SELECT * FROM iris_obs_dx_rx
+) concat
+;
---
+++ redshift-iris_parameterized_6.sql
@@ -0,0 +1,52 @@
+/*********************************************************************************
+# Copyright 2015 Observational Health Data Sciences and Informatics
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+********************************************************************************/
+/************************
+last revised: April 2016  author:  Vojtech Huser
+*************************/
+--achilles_share_level_1
+
+
+
+
+
+
+
+
+SET search_path TO  nih;
+
+select analysis_id, parameter, cast(count_value as VARCHAR) as value from
+--select * from
+(
+
+
+select 516 as analysis_id,'count of deceased' as parameter , sum(count_value) AS count_value from achilles_results where analysis_id = 501 group by analysis_id
+	  union
+--	  select 516 as analysis_id,'count of deceased' as parameter , sum(count_value) as all_cnt from achilles_results where analysis_id = 501 group by analysis_id;
+    select analysis_id, 'count of persons' as parameter, count_value as value from achilles_results where analysis_id = 1
+union
+    select analysis_id, 'count of providers' as parameter, count_value from achilles_results where analysis_id = 300
+
+) numerical
+UNION
+
+select null as analysis_id, 'measurement data start month' as parameter ,cast(min(stratum_1) as VARCHAR) as value from (
+    select stratum_1
+    --,count_value,count_value*100.0/    (select sum(count_value) from achilles_results where analysis_id = 1820 group by analysis_id) as ratio
+    from achilles_results where analysis_id = 1820
+    --exclude outlier months, months that contain less than 0.01 percent of overal data
+    and (count_value*100.0/    (select sum(count_value) from achilles_results where analysis_id = 1820 group by analysis_id)) > 0.01
+    --
+    ) a
+UNION
+select null as analysis_id, 'measurement data end  month' as parameter ,cast(max(stratum_1) as VARCHAR) as value from (
+    select stratum_1
+    from achilles_results where analysis_id = 1820
+    --exclude outlier months, months that contain less than 0.01 percent of overal data
+    and (count_value*100.0/    (select sum(count_value) from achilles_results where analysis_id = 1820 group by analysis_id)) > 0.01
+    --
+    ) a
+order by analysis_id , parameter
+;
